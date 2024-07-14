7524c9f07a4a6d47d784e5e2cbe277c30bbb23c9
Ryo Onodera <ryoqun@gmail.com>
2024-07-14 16:37:37 +0900

    save

--- src/cli.rs
+++ src/cli.rs
@@ -30,7 +30,7 @@ impl FromStr for Partition {
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         match &s.split("/").map(str::parse::<usize>).collect::<Vec<_>>()[..] {
             &[Ok(index), Ok(count)] if index <= count && index > 0 && count > 0 => Ok(Self {index, count}),
-            &other => bail!(
+            other => bail!(
                 "bad or out-of-range partition"
             ),
         }
